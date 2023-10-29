74e4972cc8b89b2e671e5d4518e3c29f41acf531
Priyash <38959321+priyashpatil@users.noreply.github.com>
2023-10-29 22:19:24 +0530

    add controller methods and routes

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
 
 class BlogPostController extends Controller
 {
-    //
+    public function index()
+    {
+        $blogPosts = BlogPost::paginate();
+
+        return view('blog-posts.index', compact('blogPosts'));
+    }
+
+    public function edit(BlogPost $blogPost)
+    {
+        return view('blog-posts.edit', compact('blogPost'));
+    }
+
+    public function update(Request $request, BlogPost $blogPost)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+
+        $blogPost->update($validated);
+
+        return redirect()->back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog-posts', [\App\Http\Controllers\BlogPostController::class, 'index'])->name('blog-posts.index');
+Route::get('/blog-posts/{blogPost}', [\App\Http\Controllers\BlogPostController::class, 'edit'])->name('blog-posts.edit');
+Route::put('/blog-posts/{blogPost}', [\App\Http\Controllers\BlogPostController::class, 'update']);
