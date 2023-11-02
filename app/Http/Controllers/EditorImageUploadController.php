525274c7690fa613a3ca790d147c1262e3ce9024
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2023-11-02 22:19:45 +0530

    add EditorImageUploadController.php

--- app/Http/Controllers/EditorImageUploadController.php
+++ app/Http/Controllers/EditorImageUploadController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class EditorImageUploadController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), ['upload' => 'required|file|image|max:5000']);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => [
+                    'message' => $validator->errors()->first('upload')
+                ]
+            ], 400);
+        }
+
+        $path = $request->file('upload')->store('uploads', ['disk' => 'public']);
+        $imageURL = Storage::disk('public')->url($path);
+
+        return response()->json(['url' => $imageURL]);
     }
 }
